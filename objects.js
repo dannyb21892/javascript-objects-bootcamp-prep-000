4019ec1b3cdc472ddc575e80882e68a3ce533b9c
Daniel Baamonde <dannyb21892@gmail.com>
2018-01-09 05:19:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,11 @@
 var playlist = {TheDearHunter: "The Lake South"}
 
-function updatePlaylist(object, artist, string) {
+function updatePlaylist(object, artist, song) {
+  object[artist] = song
+  return object
+}
+
+function removeFromPlaylist(object, artist) {
   object[artist] = string
   return object
 }
